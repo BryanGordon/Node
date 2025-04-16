c2eca398cddbba9f002e39abe44b71522d33eaba
Bryan Gordon <bryanbge92@gmail.com>
2025-04-15 22:47:00 -0500

    Create filesystem file

--- fundamentals/fs.js
+++ fundamentals/fs.js
@@ -1,1 +1,30 @@
 /* File sistem with node */
+
+/* File stats */
+const fs = require('node:fs')
+
+/*
+const stats = fs.statSync('./archivo.txt')
+
+console.log(
+  stats.isFile(), // saber si es un fichero
+  stats.isDirectory(), // saber si es directorio
+  stats.isSymbolicLink(), // saber si es un enlace simbolico
+  stats.size, // Peso en bytes
+)
+*/
+/** Read files */
+
+// const text = fs.readFileSync('./archivo.txt', 'utf-8')
+
+// Forma async usando callbacks
+
+fs.readFile('./archivo.txt', 'utf-8', (err, text) => {
+  console.log(text)
+})
+
+console.log('Ejecutando codigo mientras se ejecuta otra cosa')
+
+fs.readFile('./archivo2.txt', 'utf-8', (err, text2) => {
+  console.log(text2)
+})
