a818ac4d7fa8e85990b8ca8b01f17e92017436f8
Bryan Gordon <bryanbge92@gmail.com>
2025-04-23 18:59:30 -0500

    Create new features
    - Add new post method
    - Add validations for post method

--- api-rest/app.js
+++ api-rest/app.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const crypto =  require('node:crypto')
+const z = require('zod')
+
 const movies = require('./movies.json')
 
 const app = express()
 app.disable('x-power-by')
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.json({ message: 'Inicio del server'})
@@ -30,4 +34,53 @@ const PORT = process.env.PORT ?? 3000
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: http://localhost:${PORT}`)
+})
+
+// Metodos post
+
+app.post('/movies', (req, res) => {
+  // Hacer un schema para validar la peticion
+  const movieSchema = z.object({
+    title: z.string({
+      invalid_type_error: 'Movie title must be a string',
+      required_error: 'Movie is required'
+    }),
+    genre: z.array(z.enum(
+      ['Action', 'Adventure', 'Comedy', 'Crime', 'Drama', 'Fantasy', 'Horror', 'Thiller', 'Sci-Fi']
+    ), {
+      invalid_type_error: 'Genre must be a correct genre',
+      required_error: 'Genre is required'
+    }),
+    year: z.number().int().min(1900).max(2027),
+    director: z.string({
+      invalid_type_error: 'Director must be a string'
+    }),
+    duration: z.number().int().positive(),
+    rate: z.number().min(0).max(10),
+    poster: z.string()
+  })
+  const {
+    title,
+     genre,
+     year,
+     director,
+     duration,
+     rate,
+     poster
+  } = req.body
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    title,
+    genre,
+    year,
+    director,
+    duration,
+    rate: rate ?? 0,
+    poster
+  }
+
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie)
 })
