cf62510cf0e0b3ed3c703639c3cb41438660ca73
Bryan Gordon <bryanbge92@gmail.com>
2025-04-24 20:44:47 -0500

    Refactor movies schemas

---
+++ api-rest/schemas/movies.js
@@ -0,0 +1,34 @@
+const z = require('zod')
+
+const movieSchema = z.object({
+    title: z.string({
+      invalid_type_error: 'Movie title must be a string',
+      required_error: 'Movie is required'
+    }),
+    genre: z.array(z.enum(
+      ['Action', 'Adventure', 'Comedy', 'Crime', 'Drama', 'Fantasy', 'Horror', 'Thiller', 'Sci-Fi']
+    ), {
+      invalid_type_error: 'Genre must be a correct genre',
+      required_error: 'Genre is required'
+    }),
+    year: z.number().int().min(1900).max(2027),
+    director: z.string({
+      invalid_type_error: 'Director must be a string'
+    }),
+    duration: z.number().int().positive(),
+    rate: z.number().min(0).max(10).default(0),
+    poster: z.string()
+  })
+
+  function validateMovie (object) {
+    return movieSchema.safeParse(object)
+  }
+
+  function validatePartialMovie (object) {
+    return movieSchema.partial().safeParse(object)
+  }
+
+  module.exports = {
+    validateMovie,
+    validatePartialMovie
+  }
