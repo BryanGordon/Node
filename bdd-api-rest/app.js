e5db47f39669f91579c029023aa3ed6ba241ca41
Bryan Gordon <bryanbge92@gmail.com>
2025-05-07 22:05:09 -0500

    Refactor main file

--- bdd-api-rest/app.js
+++ bdd-api-rest/app.js
@@ -1,20 +1,22 @@
 import express, { json } from 'express'
-import { moviesRouter } from './routes/movies.js'
+import { createMovieRouter } from './routes/movies.js'
 import { corsMiddleware } from './middlewares/cors.js'
 
-const app = express()
-app.disable('x-powered-by')
-app.use(json())
-app.use(corsMiddleware())
+export const createApp = ({ movieModel }) => {
+  const app = express()
+  app.disable('x-powered-by')
+  app.use(json())
+  app.use(corsMiddleware())
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Inicio del server' })
-})
+  app.get('/', (req, res) => {
+    res.json({ message: 'Inicio del server' })
+  })
 
-app.use('/movies', moviesRouter)
+  app.use('/movies', createMovieRouter({ movieModel }))
 
-const PORT = process.env.PORT ?? 3000
+  const PORT = process.env.PORT ?? 3000
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port: http://localhost:${PORT}`)
-})
+  app.listen(PORT, () => {
+    console.log(`Server listening on port: http://localhost:${PORT}`)
+  })
+}
