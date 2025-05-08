541609aa6b8d8ee0e8d3873985f2a3fd644e3e76
Bryan Gordon <bryanbge92@gmail.com>
2025-05-07 22:08:46 -0500

    Create functions for DB

--- bdd-api-rest/models/database/mysql/movie.js
+++ bdd-api-rest/models/database/mysql/movie.js
@@ -12,15 +12,16 @@ const connection = await mysql.createConnection(config)
 
 export class MovieModel {
   static getAll = async ({ genre }) => {
-    const result = await connection.query(
+    const [result] = await connection.query(
       'SELECT * FROM movie;'
     )
-    console.log(result)
+
+    return result
   }
 
   static getById = async ({ id }) => {
     const [movies] = await connection.query(
-      'SELECT title, year, director, duration, poster, rate, id FROM movie WHERE id = id;'
+      'SELECT * FROM movie WHERE id = ?;', [id]
     )
 
     if (movies.length === 0) return null
@@ -29,14 +30,35 @@ export class MovieModel {
   }
 
   static create = async ({ object }) => {
+    const {
+      title,
+      year,
+      director,
+      duration,
+      poster,
+      rate
+    } = object
 
+    const [uuidResult] = await connection.query('SELECT UUID() uuid;')
+    const [{ uuid }] = uuidResult
+    try {
+      await connection.query(
+        'INSERT INTO movie VALUE(UUID_TO_BIN(?), ?, ? , ?, ?, ?, ?);', [uuid, title, year, director, duration, poster, rate]
+      )
+    } catch (e) {
+      throw new Error('Error creating new movie')
+    }
+
+    const [movie] = await connection.query('SELECT * FROM movie WHERE id = ?;', [uuid])
+
+    return movie[0]
   }
 
   static delete = async ({ id }) => {
-
+    // To do + To do search by genre
   }
 
   static update = async ({ id, object }) => {
-
+    // To do
   }
 }
