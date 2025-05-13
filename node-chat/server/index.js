7db613e1de8d379e4b689462ffef7c5c4fd3d6ca
Bryan Gordon <bryanbge92@gmail.com>
2025-05-12 19:00:47 -0500

    Create server connection

---
+++ node-chat/server/index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import logger from 'morgan'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
+const PORT = process.env.PORT ?? 3000
+
+const app = express()
+app.use(logger('dev'))  
+
+const server = createServer(app)
+const io = new Server(server)
+io.on('connection', () => {
+  console.log('A user has connected!')
+})
+
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/client/index.html')
+})
+
+server.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`)
+})
