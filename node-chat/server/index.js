3250876675a379ef0a78cd2cc4df5f8fbace3e00
Bryan Gordon <bryanbge92@gmail.com>
2025-05-14 22:11:18 -0500

    Create DB connection

--- node-chat/server/index.js
+++ node-chat/server/index.js
@@ -2,14 +2,32 @@ import express from 'express'
 import logger from 'morgan'
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
 
 const PORT = process.env.PORT ?? 3000
+
+dotenv.config()
+
+const db = createClient({
+  url: "libsql://node-chat-bryangordon.aws-us-east-1.turso.io",
+  authToken: process.env.DB_TOKEN
+})
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT
+  )
+`)
 
 const app = express()
 app.use(logger('dev'))  
 
 const server = createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+  connectionStateRecovery: { }
+})
 
 io.on('connection', (socket) => {
   console.log('A user has connected!')
@@ -18,8 +36,19 @@ io.on('connection', (socket) => {
     console.log('An user has disconnected.')
   })
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg)
+  socket.on('chat message',  async(msg) => {
+    let results
+
+    try {
+      results = await db.execute({
+       sql: 'INSERT INTO messages (content) VALUES (:content)',
+       args: { content: msg }
+      }) 
+    } catch (e) {
+      console.error(e)
+    }
+
+    io.emit('chat message', msg, results.lastInsertRowid.toString())
   })
 })
 
